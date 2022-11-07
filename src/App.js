ac7003fdefa8599edb468faa1b03c14ca8cb7fe5
Edward Saviano <lyonz4prez@gmail.com>
2022-11-07 13:26:36 -0500

    heading component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Heading () {
-  return (
-  <h1>This is an h1 heading</h1>
-  )
-}
+import Heading from './components/Heading';
+
 function App() {
     return (
     <div className="App">
-    This is the starting code for "Your first component" ungraded lab
     <Heading />
     </div>
     );
