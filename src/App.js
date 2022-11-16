41291856d00b41012431818636069508147165e1
Edward Saviano <lyonz4prez@gmail.com>
2022-11-16 13:07:59 -0500

    dynamic events

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-import Card from './Card';
 
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+    }
+    
     return (
-      <div className="App">
-      <h1>Task: Add three Card elements</h1>
-      <Card h2="First card's h2" h3="First card's h3" />
-      <Card h2="Second card's h2" h3="Second card's h3" />
-      <Card h2="Third card's h2" h3="Third card's h3" />
-  </div>
-  );
-};
+      <div>
+        <h1>Task: Add a button and handle a click event</h1>
+          <button onClick={handleClick}>Guess the number between 1 and 3</button>
+      </div>
+    );
+}
 
 export default App;
